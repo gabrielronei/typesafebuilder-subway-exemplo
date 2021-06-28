a0c576e4d897e6263521924315c302ec36ee9287
Gabriel Ronei <gabriheron@hotmail.com>
2021-06-28 12:32:20 -0300

    Adicionando imports estaticos para deixar mais legivel ainda

--- src/br/com/gabriels/subway/Main.java
+++ src/br/com/gabriels/subway/Main.java
@@ -1,11 +1,16 @@
 package br.com.gabriels.subway;
 
+import static br.com.gabriels.subway.Pao.*;
+import static br.com.gabriels.subway.Queijo.*;
+import static br.com.gabriels.subway.Recheio.*;
+import static br.com.gabriels.subway.Vegetal.*;
+
 public class Main {
     public static void main(String[] args) {
-        Lanche lanche = Lanche.umLanche(Recheio.CHURRASCO, Pao.PARMESAO_OREGANO)
+        Lanche lanche = Lanche.umLanche(CHURRASCO, PARMESAO_OREGANO)
                 .grande()
-                .comQueijo(Queijo.PRATO)
-                .comVegetais(Vegetal.CEBOLA, Vegetal.ALFACE)
+                .comQueijo(PRATO)
+                .comVegetais(CEBOLA, ALF    ACE)
                 .comMolho(Molho.CHIPOTLE)
                 .build();
 
